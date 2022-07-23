7d916c51b49ed32fa1341948cd1be86784d362b1
Dolphin Oracle <dolphinoracle@gmail.com>
2022-07-23 10:24:48 -0400

    translation refresh

---
+++ translations/mx-dockmaker_th.ts
@@ -0,0 +1,469 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.0" language="th">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Program_Name</source>
+        <translation>Program_Name</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="29"/>
+        <source>Usage</source>
+        <translation>การใช้งาน</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="106"/>
+        <source>Dock Preview</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="118"/>
+        <source>Add New Application</source>
+        <translation>เพิ่มแอปพลิเคชันใหม่</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="131"/>
+        <source>Command</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="301"/>
+        <source>Add application</source>
+        <translation>เพิ่มแอปพลิเคชัน</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="393"/>
+        <source>Delete this application</source>
+        <translation>ลบแอปพลิเคชันนี้</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="124"/>
+        <source>Border</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="154"/>
+        <source>Size</source>
+        <translation>ขนาด</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="161"/>
+        <source>Background</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="379"/>
+        <source>Back</source>
+        <translation>ย้อนกลับ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="184"/>
+        <source>File</source>
+        <translation>ไฟล์</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="216"/>
+        <location filename="mainwindow.cpp" line="708"/>
+        <location filename="mainwindow.cpp" line="735"/>
+        <source>Select icon...</source>
+        <translation>เลือกไอคอน...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="229"/>
+        <location filename="mainwindow.cpp" line="82"/>
+        <location filename="mainwindow.cpp" line="582"/>
+        <location filename="mainwindow.cpp" line="711"/>
+        <source>Select...</source>
+        <translation>เลือก...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="315"/>
+        <source>Move right</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="328"/>
+        <source>Move left</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="439"/>
+        <source>Display help </source>
+        <translation>แสดงหน้าช่วยเหลือ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="442"/>
+        <source>Help</source>
+        <translation>ช่วยเหลือ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="449"/>
+        <source>Alt+H</source>
+        <translation>Alt+H</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="478"/>
+        <source>About this application</source>
+        <translation>เกี่ยวกับแอปพลิเคชันนี้</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="481"/>
+        <source>About...</source>
+        <translation>เกี่ยวกับ...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="488"/>
+        <source>Alt+B</source>
+        <translation>Alt+B</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="520"/>
+        <source>Quit application</source>
+        <translation>ออกจากแอปพลิเคชัน</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="523"/>
+        <source>Close</source>
+        <translation>ปิด</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="530"/>
+        <source>Alt+N</source>
+        <translation>Alt+N</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="571"/>
+        <source>Save</source>
+        <translation>บันทึก</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="103"/>
+        <source>1. Add applications to the dock one at a time
+2. Select a .desktop file or enter a command for the application you want
+3. Select icon attributes for size, background (black is standard) and border
+4. Press &quot;Add application&quot; to continue or &quot;Save&quot; to finish</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="126"/>
+        <location filename="mainwindow.cpp" line="590"/>
+        <source>black</source>
+        <translation>สีดำ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="127"/>
+        <location filename="mainwindow.cpp" line="591"/>
+        <source>white</source>
+        <translation>สีขาว</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="137"/>
+        <source>This tool allows you to create a new dock with one or more applications. You can also edit or delete a dock created earlier.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="139"/>
+        <source>Operation mode</source>
+        <translation>โหมดการทำงาน</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="140"/>
+        <source>&amp;Close</source>
+        <translation>&amp;ปิด</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="141"/>
+        <source>&amp;Move</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="142"/>
+        <location filename="mainwindow.cpp" line="295"/>
+        <source>&amp;Delete</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="143"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="144"/>
+        <source>C&amp;reate</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="220"/>
+        <source>Dock name</source>
+        <translation>ชื่อ Dock</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="221"/>
+        <source>Enter the name to show in the Menu:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="293"/>
+        <source>Select dock to delete</source>
+        <translation>เลือก Dock ที่จะลบ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="294"/>
+        <source>Confirmation</source>
+        <translation>การยืนยัน</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="295"/>
+        <source>Are you sure you want to delete %1?</source>
+        <translation>คุณแน่ใจหรือไม่ที่จะลบ %1?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="295"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;ยกเลิก</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="353"/>
+        <source>Next</source>
+        <translation>ต่อไป</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="320"/>
+        <source>Select dock to move</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="293"/>
+        <location filename="mainwindow.cpp" line="320"/>
+        <location filename="mainwindow.cpp" line="454"/>
+        <location filename="mainwindow.cpp" line="657"/>
+        <source>Dock Files (*.mxdk);;All Files (*.*)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="330"/>
+        <location filename="mainwindow.cpp" line="351"/>
+        <location filename="mainwindow.cpp" line="666"/>
+        <source>Could not open file</source>
+        <translation>ไม่สามารถเปิดไฟล์ได้</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="453"/>
+        <source>Overwrite?</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="453"/>
+        <source>Do you want to overwrite the dock file?</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="454"/>
+        <source>Save file</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="492"/>
+        <source>Dock saved</source>
+        <translation>บันทึก Dock แล้ว</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="492"/>
+        <source>The dock has been saved.
+
+To edit the newly created dock please select &apos;Edit an existing dock&apos;.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="511"/>
+        <source>About %1</source>
+        <translation>เกี่ยวกับ %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="513"/>
+        <source>Version: </source>
+        <translation>เวอร์ชัน:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="514"/>
+        <source>Description goes here</source>
+        <translation>ใส่คำอธิบายที่นี่</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="516"/>
+        <source>Copyright (c) MX Linux</source>
+        <translation>สงวนลิขสิทธิ์ (c) MX Linux</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="517"/>
+        <source>%1 License</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="526"/>
+        <source>%1 Help</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="638"/>
+        <source>Select .desktop file</source>
+        <translation>เลือกไฟล์ .desktop</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="638"/>
+        <source>Desktop Files (*.desktop)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="657"/>
+        <source>Select a dock file</source>
+        <translation>เลือกไฟล์ Dock</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="660"/>
+        <source>No file selected</source>
+        <translation>ไม่มีไฟล์ที่เลือก</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="660"/>
+        <source>You haven't selected any dock file to edit.
+Creating a new dock instead.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="666"/>
+        <source>Could not open selected file.
+Creating a new dock instead.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="675"/>
+        <source>1. Edit applications one at a time using the Back and Next buttons
+2. Add or delete applications as you like
+3. When finished click Save</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="719"/>
+        <source>Select icon</source>
+        <translation>เลือกไอคอน</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="719"/>
+        <source>Icons (*.png *.jpg *.bmp *.xpm *.svg)</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>PickLocation</name>
+    <message>
+        <location filename="picklocation.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="23"/>
+        <source>Dock Location</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="29"/>
+        <source>TopCenter</source>
+        <translation>บนกลาง</translation>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="45"/>
+        <source>BottomLeft</source>
+        <translation>ล่างซ้าย</translation>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="61"/>
+        <source>BottomCenter</source>
+        <translation>ล่างกลาง</translation>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="80"/>
+        <source>TopLeft</source>
+        <translation>บนซ้าย</translation>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="96"/>
+        <source>TopRight</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="112"/>
+        <source>LeftCenter</source>
+        <translation>ซ้ายกลาง</translation>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="128"/>
+        <source>RightCenter</source>
+        <translation>ขวากลาง</translation>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="144"/>
+        <source>BottomRight</source>
+        <translation>ล่างขวา</translation>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="160"/>
+        <source>RightTop</source>
+        <translation>ขวาบน</translation>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="173"/>
+        <source>LeftTop</source>
+        <translation>ซ้ายบน</translation>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="189"/>
+        <source>LeftBottom</source>
+        <translation>ซ้ายล่าง</translation>
+    </message>
+    <message>
+        <location filename="picklocation.ui" line="205"/>
+        <source>RightBottom</source>
+        <translation>ขวาล่าง</translation>
+    </message>
+    <message>
+        <location filename="picklocation.cpp" line="9"/>
+        <source>Select dock location</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="about.cpp" line="32"/>
+        <source>License</source>
+        <translation>สัญญาอนุญาต</translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="33"/>
+        <location filename="about.cpp" line="43"/>
+        <source>Changelog</source>
+        <translation>Changelog</translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="34"/>
+        <source>Cancel</source>
+        <translation>ยกเลิก</translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="51"/>
+        <source>&amp;Close</source>
+        <translation>&amp;ปิด</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="57"/>
+        <source>Error</source>
+        <translation>ข้อผิดพลาด</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="58"/>
+        <source>You must run this program as normal user.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
